b22766adbe4f584d9547aeff65ffed6439103426
Joe <burtonjae@hotmail.co.uk>
2019-07-27 22:21:01 +0100

    Add admin check to LCG command

--- src/main/java/kaptainwutax/monkey/command/CommandLcg.java
+++ src/main/java/kaptainwutax/monkey/command/CommandLcg.java
@@ -4,6 +4,7 @@ import kaptainwutax.monkey.holder.HolderGuild;
 import kaptainwutax.monkey.init.Commands;
 import kaptainwutax.monkey.init.Guilds;
 import kaptainwutax.monkey.utility.Log;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class CommandLcg extends Command {
@@ -19,7 +20,7 @@ public class CommandLcg extends Command {
     @Override
     public void processCommand(MessageReceivedEvent message, String rawCommand) {
         HolderGuild server = Guilds.instance().getOrCreateServer(new HolderGuild(message.getGuild()));
-        if (!server.controller.funCommands)
+        if (!server.controller.funCommands && !message.getMember().hasPermission(Permission.ADMINISTRATOR))
             return;
         
         rawCommand = removePrefix(rawCommand);
